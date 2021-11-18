450e70542da644a27384cba78d4367693e123b14
alexanderolsson <alexander.00.olsson@gmail.com>
2021-11-18 15:33:20 +0100

    gas and break

--- src/Car.java
+++ src/Car.java
@@ -61,11 +61,13 @@ public abstract class Car implements Movable {
         currentSpeed = 0;
     }
     // (TODO fix this method according to lab pm)
-    public void gas(double amount){
-        incrementSpeed(amount);
+    public void gas(double amount) {
+        if(amount >= 0 && amount <= 1) {
+            incrementSpeed(amount);}
     }
     // TODO fix this method according to lab pm
     public void brake(double amount){
+        if (amount >= 0 && amount <= 1)
         decrementSpeed(amount);
     }
     //Not OCP, but how is it supposed to be??
